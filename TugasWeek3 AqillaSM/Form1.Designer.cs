1b2f0442e91fdcafc31e3babf08c310981158794
mahazoya <mahazoya@DESKTOP-ES7HCLB>
2022-03-13 21:33:18 +0700

    Add project files.

---
+++ TugasWeek3 AqillaSM/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TugasWeek3_AqillaSM
+{
+    partial class FormWarna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelInput = new System.Windows.Forms.Label();
+            this.TBoxInput = new System.Windows.Forms.TextBox();
+            this.ButtonProses = new System.Windows.Forms.Button();
+            this.LabelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelInput
+            // 
+            this.LabelInput.AutoSize = true;
+            this.LabelInput.Location = new System.Drawing.Point(55, 49);
+            this.LabelInput.Name = "LabelInput";
+            this.LabelInput.Size = new System.Drawing.Size(112, 25);
+            this.LabelInput.TabIndex = 0;
+            this.LabelInput.Text = "Input Data :";
+            // 
+            // TBoxInput
+            // 
+            this.TBoxInput.Location = new System.Drawing.Point(60, 77);
+            this.TBoxInput.Name = "TBoxInput";
+            this.TBoxInput.Size = new System.Drawing.Size(515, 30);
+            this.TBoxInput.TabIndex = 1;
+            this.TBoxInput.TextChanged += new System.EventHandler(this.TBoxInput_TextChanged);
+            // 
+            // ButtonProses
+            // 
+            this.ButtonProses.Location = new System.Drawing.Point(60, 154);
+            this.ButtonProses.Name = "ButtonProses";
+            this.ButtonProses.Size = new System.Drawing.Size(95, 32);
+            this.ButtonProses.TabIndex = 2;
+            this.ButtonProses.Text = "Proses";
+            this.ButtonProses.UseVisualStyleBackColor = true;
+            this.ButtonProses.Click += new System.EventHandler(this.ButtonProses_Click);
+            // 
+            // LabelOutput
+            // 
+            this.LabelOutput.AutoSize = true;
+            this.LabelOutput.Location = new System.Drawing.Point(60, 234);
+            this.LabelOutput.Name = "LabelOutput";
+            this.LabelOutput.Size = new System.Drawing.Size(93, 25);
+            this.LabelOutput.TabIndex = 3;
+            this.LabelOutput.Text = "[EMPTY]";
+            this.LabelOutput.Click += new System.EventHandler(this.LabelOutput_Click);
+            // 
+            // FormWarna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 382);
+            this.Controls.Add(this.LabelOutput);
+            this.Controls.Add(this.ButtonProses);
+            this.Controls.Add(this.TBoxInput);
+            this.Controls.Add(this.LabelInput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(100, 100);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "FormWarna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Pengaturan Warna";
+            this.Load += new System.EventHandler(this.FormWarna_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelInput;
+        private System.Windows.Forms.TextBox TBoxInput;
+        private System.Windows.Forms.Button ButtonProses;
+        private System.Windows.Forms.Label LabelOutput;
+    }
+}
+
---
+++ TugasWeek3 AqillaSM/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace TugasWeek3_AqillaSM
+{
+    public partial class FormWarna : System.Windows.Forms.Form
+    {
+        public FormWarna()
+        {
+            InitializeComponent();
+        }
+
+        private void FormWarna_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string Kata = "";
+        string CekKataIsi = "";
+        string LuarKataIsi = "";
+        string CekWarna = "";
+        string LuarCekWarna = "";
+        
+
+        public void TBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            Kata = TBoxInput.Text;
+            Kata = Kata.ToUpper();
+        }
+        
+        private void LabelOutput_Click(object sender, EventArgs e)
+        {
+           
+        }
+        static public Font ChangeFontSize(Font font, float fontSize)
+        {
+            if (font != null)
+            {
+                float currentSize = font.Size;
+                if (currentSize != fontSize)
+                {
+                    font = new Font(font.Name, fontSize,
+                        font.Style, font.Unit,
+                        font.GdiCharSet, font.GdiVerticalFont);
+                }
+            }
+            return font;
+        }
+
+        String Input = "";
+        String Kosong = "[EMPTY]";
+
+        public void ButtonProses_Click(object sender, EventArgs e)
+        {
+            Input = TBoxInput.Text;
+            Input = Input.ToUpper();
+            if (Input == "RESTART")
+            {
+                LabelOutput.Font = ChangeFontSize(LabelOutput.Font, 12);
+                LabelOutput.ForeColor = Color.Black;
+                LabelOutput.Text = Kosong;
+                
+            }
+            else if (Input == "HIDE")
+            {
+                LabelOutput.Hide();
+            }
+            else if (Input == "SHOWN")
+            {
+                LabelOutput.Show();
+            }
+            else if (Input == "BESARKAN")
+            {
+                LabelOutput.Font = ChangeFontSize(LabelOutput.Font, LabelOutput.Font.Size + 2);
+            }
+            else if (Input == "KECILKAN")
+            {
+                LabelOutput.Font = ChangeFontSize(LabelOutput.Font, LabelOutput.Font.Size - 2);
+            }
+            else if (Kata.Length > 3)
+            {
+                CekKataIsi = Kata.Substring(0, 4);
+                LuarKataIsi = Kata.Substring(4);
+                if (CekKataIsi == "ISI:")
+                {
+                    LabelOutput.Text = LuarKataIsi;
+                }
+                else if (Kata.Length > 5)
+                {
+                    CekWarna = Kata.Substring(0, 6);
+                    LuarCekWarna = Kata.Substring(6);
+                    if (CekWarna == "WARNA:")
+                    {
+                        if (LuarCekWarna == "DEFAULT")
+                        {
+                            LabelOutput.ForeColor = Color.Black;
+                        }
+                        else if (LuarCekWarna == "MERAH")
+                        {
+                            LabelOutput.ForeColor = Color.Red;
+                        }
+                        else if (LuarCekWarna == "HIJAU")
+                        {
+                            LabelOutput.ForeColor = Color.Green;
+                        }
+                        else if(LuarCekWarna == "BIRU")
+                        {
+                            LabelOutput.ForeColor = Color.Blue;
+                        }
+                        else
+                        {
+                            MessageBox.Show("WARNA TIDAK TERDAFTAR");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("SYNTAX TIDAK TERDAFTAR!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("SYNTAX TIDAK TERDAFTAR!");
+                }
+
+            }    
+            else
+            {
+                MessageBox.Show("SYNTAX TIDAK TERDAFTAR!");
+            }    
+        }
+    }
+}
